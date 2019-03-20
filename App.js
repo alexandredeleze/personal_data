5cecb4b2c06f60f69a689a95dddcba66b8823a33
escariche <javiescariz.je@gmail.com>
2019-03-20 18:05:01 +0100

    nav

--- App.js
+++ App.js
@@ -4,16 +4,7 @@ import TabNavigator from "./Navigation/Navigation";
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-        <ImageBackground source={require("./resources/background.png")} style={{width: '100%', height: '100%'}}>
-          <View style={styles.container}>
-            <Text>Hey ho!</Text>
-          </View>
-        </ImageBackground>
-=======
         <TabNavigator/>
->>>>>>> fa3ab040762378fb066424a0151c2aab6ece57ef
     );
   }
 }
-
