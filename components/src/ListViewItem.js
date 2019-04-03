3aa3f64c4612605c1404c830d8fd7a1d02a69c6f
Alexandre Deleze <alexandre.deleze@epfl.ch>
2019-04-03 11:17:51 +0200

    Adaptation of input interface

--- components/src/ListViewItem.js
+++ components/src/ListViewItem.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {View, Text, Dimensions} from 'react-native';
 import CheckBox from './CheckBox';
 import UtilsRedux from "./UtilsRedux";
 
@@ -17,17 +16,10 @@ class ListViewItem extends Component {
 
     render() {
         let data = this.props.data;
-        let color = '#000';//data.completed ? '#C5C8C9' : '#000';
-        let textDecorationLine = 'none'; //data.completed ? 'line-through' : 'none';
         return (
-            //<TouchableHighlight underlayColor={'#eee'}
-            // style={{paddingTop: 6, paddingBottom: 6, backgroundColor: "#F8F8F8", borderBottomWidth:1, borderColor: '#eee'}} {...this.props.sortHandlers}>
-            <View style={{width: Dimensions.get('window').width-10, paddingRight: 10, paddingLeft: 10, paddingTop: 10, paddingBottom: 10, backgroundColor: 'rgba(999, 999, 999, 0.4)',//"#F8F8F8",
-                borderBottomWidth:1, borderColor: '#eee', flexDirection: 'row', alignItems: 'center'}}>
-                <CheckBox data={data} color={color} onCheckBoxPressed={this._onCheckBoxPressed}></CheckBox>
-                <Text style={{fontSize:18, color: color, textDecorationLine: textDecorationLine}}>{data.title}</Text>
-            </View>
-            //</TouchableHighlight>
+
+            <CheckBox data={data} onCheckBoxPressed={this._onCheckBoxPressed}/>
+
         )
     }
 }
